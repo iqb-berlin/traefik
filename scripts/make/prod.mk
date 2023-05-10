6a8249b3d920a307d3ca69f6611679f15703c79d
svwolter <sven.wolter@iqb.hu-berlin.de>
2023-05-10 20:54:03 +0200

    Rename production files to avoid application naming conflicts

--- scripts/make/prod.mk
+++ scripts/make/prod.mk
@@ -1,19 +1,19 @@
 BASE_DIR := $(shell git rev-parse --show-toplevel)
 
-include $(BASE_DIR)/.env.prod
+include $(BASE_DIR)/.env.traefik
 
-## exports all variables (especially those of the included .env.prod file!)
+## exports all variables (especially those of the included .env.traefik file!)
 .EXPORT_ALL_VARIABLES:
 
 ## prevents collisions of make target names with possible file names
-.PHONY: prod-ramp-up prod-shut-down prod-start prod-stop prod-status prod-logs prod-config prod-system-prune\
-	prod-volumes-prune prod-images-clean
+.PHONY: traefik-up traefik-down traefik-start traefik-stop traefik-status traefik-logs traefik-config\
+	traefik-system-prune traefik-volumes-prune traefik-images-clean
 
 ## disables printing the recipe of a make target before executing it
-.SILENT: prod-images-clean
+.SILENT: traefik-images-clean
 
 ## Pull newest images, create and start docker containers
-prod-ramp-up:
+traefik-up:
 	@if\
 		! test -f $(BASE_DIR)/secrets/traefik/studio-lite.crt ||\
 		! test -f $(BASE_DIR)/secrets/traefik/studio-lite.key ||\
@@ -31,78 +31,78 @@ prod-ramp-up:
 					echo "===============================================";\
 	fi
 	docker compose\
-			-f $(BASE_DIR)/docker-compose.yml\
-			-f $(BASE_DIR)/docker-compose.prod.yml\
-			--env-file $(BASE_DIR)/.env.prod\
+			-f $(BASE_DIR)/docker-compose.traefik.yml\
+			-f $(BASE_DIR)/docker-compose.traefik.prod.yml\
+			--env-file $(BASE_DIR)/.env.traefik\
 		pull
 	docker compose\
-			-f $(BASE_DIR)/docker-compose.yml\
-			-f $(BASE_DIR)/docker-compose.prod.yml\
-			--env-file $(BASE_DIR)/.env.prod\
+			-f $(BASE_DIR)/docker-compose.traefik.yml\
+			-f $(BASE_DIR)/docker-compose.traefik.prod.yml\
+			--env-file $(BASE_DIR)/.env.traefik\
 		up -d
 
 ## Stop and remove docker containers
-prod-shut-down:
+traefik-down:
 	docker compose\
-  		-f $(BASE_DIR)/docker-compose.yml\
-  		-f $(BASE_DIR)/docker-compose.prod.yml\
-  		--env-file $(BASE_DIR)/.env.prod\
-  	down
+			-f $(BASE_DIR)/docker-compose.traefik.yml\
+			-f $(BASE_DIR)/docker-compose.traefik.prod.yml\
+			--env-file $(BASE_DIR)/.env.traefik\
+		down
 
 ## Start docker containers
-# Param (optional): SERVICE - Start the specified service only, e.g. `make prod-start SERVICE=grafana`
-prod-start:
+# Param (optional): SERVICE - Start the specified service only, e.g. `make traefik-start SERVICE=grafana`
+traefik-start:
 	docker compose\
-			-f $(BASE_DIR)/docker-compose.yml\
-			-f $(BASE_DIR)/docker-compose.prod.yml\
-		 	--env-file $(BASE_DIR)/.env.prod\
+			-f $(BASE_DIR)/docker-compose.traefik.yml\
+			-f $(BASE_DIR)/docker-compose.traefik.prod.yml\
+			--env-file $(BASE_DIR)/.env.traefik\
 		start $(SERVICE)
 
 ## Stop docker containers
-prod-stop:
+traefik-stop:
 	docker compose\
-			-f $(BASE_DIR)/docker-compose.yml\
-			-f $(BASE_DIR)/docker-compose.prod.yml\
-		 	--env-file $(BASE_DIR)/.env.prod\
+			-f $(BASE_DIR)/docker-compose.traefik.yml\
+			-f $(BASE_DIR)/docker-compose.traefik.prod.yml\
+			--env-file $(BASE_DIR)/.env.traefik\
 		stop $(SERVICE)
 
 ## Show status of containers
-# Param (optional): SERVICE - Show status of the specified service only, e.g. `make prod-status SERVICE=grafana`
-prod-status:
+# Param (optional): SERVICE - Show status of the specified service only, e.g. `make traefik-status SERVICE=grafana`
+traefik-status:
 	docker compose\
-			-f $(BASE_DIR)/docker-compose.yml\
-			-f $(BASE_DIR)/docker-compose.prod.yml\
-		 	--env-file $(BASE_DIR)/.env.prod\
+			-f $(BASE_DIR)/docker-compose.traefik.yml\
+			-f $(BASE_DIR)/docker-compose.traefik.prod.yml\
+			--env-file $(BASE_DIR)/.env.traefik\
 		ps -a $(SERVICE)
 
 ## Show service logs
-# Param (optional): SERVICE - Show log of the specified service only, e.g. `make prod-logs SERVICE=grafana`
-prod-logs:
+# Param (optional): SERVICE - Show log of the specified service only, e.g. `make traefik-logs SERVICE=grafana`
+traefik-logs:
 	docker compose\
-			-f $(BASE_DIR)/docker-compose.yml\
-			-f $(BASE_DIR)/docker-compose.prod.yml\
-		 	--env-file $(BASE_DIR)/.env.prod\
+			-f $(BASE_DIR)/docker-compose.traefik.yml\
+			-f $(BASE_DIR)/docker-compose.traefik.prod.yml\
+			--env-file $(BASE_DIR)/.env.traefik\
 		logs -f $(SERVICE)
 
 ## Show services configuration
-# Param (optional): SERVICE - Show config of the specified service only, e.g. `make prod-config SERVICE=grafana`
-prod-config:
+# Param (optional): SERVICE - Show config of the specified service only, e.g. `make traefik-config SERVICE=grafana`
+traefik-config:
 	docker compose\
-			-f $(BASE_DIR)/docker-compose.yml\
-			-f $(BASE_DIR)/docker-compose.prod.yml\
-			--env-file $(BASE_DIR)/.env.prod\
+			-f $(BASE_DIR)/docker-compose.traefik.yml\
+			-f $(BASE_DIR)/docker-compose.traefik.prod.yml\
+			--env-file $(BASE_DIR)/.env.traefik\
 		config $(SERVICE)
 
 ## Remove unused dangling images, containers, networks, etc. Data volumes will stay untouched!
-prod-system-prune:
+traefik-system-prune:
 	docker system prune
 
 ## Remove all anonymous local volumes not used by at least one container.
-prod-volumes-prune:
+traefik-volumes-prune:
 	docker volume prune
 
 ## Remove all unused (not just dangling) images!
-prod-images-clean:
+traefik-images-clean:
 	if test "$(shell docker images -q grafana/grafana)"; then docker rmi $(shell docker images -q grafana/grafana); fi
 	if test "$(shell docker images -q prom/prometheus)"; then docker rmi $(shell docker images -q prom/prometheus); fi
 	if test "$(shell docker images -q traefik)"; then docker rmi $(shell docker images -q traefik); fi
