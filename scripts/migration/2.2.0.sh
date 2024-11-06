d0f084bedf584d82c78a04acad951b656192a64d
svwolter <sven.wolter@iqb.hu-berlin.de>
2024-11-06 20:23:54 +0100

    Fix migration 2.2.0 script

--- scripts/migration/2.2.0.sh
+++ scripts/migration/2.2.0.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
 migrate_file_structure() {
-    if [ -f secrets/traefik/certificate.pem ]; then
-      mv secrets/traefik/certificate.pem secrets/traefik/certs/certificate.pem
-    fi
-    if [ -f secrets/traefik/privkey.pem ]; then
-      mv secrets/traefik/privkey.pem secrets/traefik/certs/private_key.pem
-    fi
+  printf "Migrate application file structure ...\n"
+  if [ -f secrets/traefik/certificate.pem ]; then
+    mv secrets/traefik/certificate.pem secrets/traefik/certs/certificate.pem
+  fi
+  if [ -f secrets/traefik/privkey.pem ]; then
+    mv secrets/traefik/privkey.pem secrets/traefik/certs/private_key.pem
+  fi
+  if [ -f scripts/update.sh ]; then
+    mv scripts/update.sh scripts/update_traefik.sh
+  fi
+  if ! [ -f Makefile ]; then
+    printf "include %s/scripts/make/traefik.mk\n" "$(pwd)" >"$(pwd)/Makefile"
+  fi
+  printf "Application file structure migrated.\n\n"
 }
 
 update_environment_file() {
+  printf "Update docker environment file ...\n"
   sed -i.bak "/HTTPS_PORT=.*/a\\\n## TLS Certificates Resolvers" .env.traefik && rm .env.traefik.bak
   sed -i.bak "/## TLS Certificates Resolvers/a\# Choose '', if you handle certificates manually, or" \
     .env.traefik && rm .env.traefik.bak
@@ -20,14 +29,17 @@ update_environment_file() {
     .env.traefik && rm .env.traefik.bak
   sed -i.bak "/TLS_ACME_CA_SERVER=.*/a\TLS_ACME_EAB_KID=''\nTLS_ACME_EAB_HMAC_ENCODED=''" \
     .env.traefik && rm .env.traefik.bak
-  sed -i.bak "/TLS_ACME_EAB_HMAC_ENCODED=.*/a\TLS_ACME_EMAIL=admin.name@organisation.org\n" \
+  sed -i.bak "/TLS_ACME_EAB_HMAC_ENCODED=.*/a\TLS_ACME_EMAIL=admin.name@organisation.org" \
     .env.traefik && rm .env.traefik.bak
+  printf "Docker environment file updated.\n\n"
 }
 
 clean_up() {
+  printf "Clean up obsolete files ...\n"
   if [ -f config/traefik/tls-config.yaml ]; then
     rm config/traefik/tls-config.yaml
   fi
+  printf "Obsolete files cleaned up.\n\n"
 }
 
 main() {
