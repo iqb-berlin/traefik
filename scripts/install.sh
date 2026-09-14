952d46e45bf4b7df8b084d7fcb502f3a8b5b2d86
svwolter <sven.wolter@iqb.hu-berlin.de>
2026-09-14 18:42:59 +0200

    Allow Keycloak import/export for IP based hostnames

--- scripts/install.sh
+++ scripts/install.sh
@@ -456,7 +456,7 @@ convert_bootstrap_to_permanent_admin() {
       --env-file ".env.${APP_NAME}" \
       --file "docker-compose.${APP_NAME}.yaml" \
       --file "docker-compose.${APP_NAME}.prod.yaml" \
-		run --rm --name traefik-keycloak-realm-export --user "$(id -u):0" \
+		run --rm --name traefik-keycloak-realm-export --user "$(id -u):0" --env KC_HOSTNAME=localhost \
 			keycloak\
 				export --file /opt/keycloak/data/export/master-realm.json --realm master &>/dev/null
 	  docker compose \
@@ -474,7 +474,7 @@ convert_bootstrap_to_permanent_admin() {
       --env-file ".env.${APP_NAME}" \
       --file "docker-compose.${APP_NAME}.yaml" \
       --file "docker-compose.${APP_NAME}.prod.yaml" \
-		run --rm --name traefik-keycloak-realm-export --user "$(id -u):0" \
+		run --rm --name traefik-keycloak-realm-export --user "$(id -u):0" --env KC_HOSTNAME=localhost \
 			keycloak\
 				import --file /opt/keycloak/data/export/master-realm.json &>/dev/null
   docker compose \
